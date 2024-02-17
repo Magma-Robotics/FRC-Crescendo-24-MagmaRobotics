b0c562efb4c9615488940aabfeadd87b7076479c
NelsonWong2026 <1361400308@k12.hi.us>
2024-02-16 18:44:21 -1000

    added auto movement

--- src/main/java/frc/robot/commands/drive/AutoMovement.java
+++ src/main/java/frc/robot/commands/drive/AutoMovement.java
@@ -1,42 +1,40 @@
 package frc.robot.commands.drive;
 
-/*import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
 
 public class AutoMovement extends Command {
     private final DriveTrain driveTrain;
-    private final XboxController driveController;
     private double leftPower, rightPower, duration;
 
-    public AutoMovement(DriveTrain driveTrain, double duration, double leftPower, double rightPower){
+    public AutoMovement(DriveTrain driveTrain, double duration, double leftPower, double rightPower) {
         this.driveTrain = driveTrain;
-        this.driveController = driveController;
-        driveController = new XboxController(Constants.OperatorConstants.kDriverControllerPort);
+        this.duration = duration;
+        this.leftPower = leftPower;
+        this.rightPower = rightPower;
         addRequirements(driveTrain);
     }
     
     @Override
     public void initialize() {
+        double currentTime = System.currentTimeMillis() / 1000;
+        duration = currentTime + duration;
     }
 
     @Override
     public void execute() {
-        this.driveTrain.diffDrive(
-            driveController.getLeftY(), driveController.getRightY()
-            );     
+        driveTrain.diffDrive(leftPower, rightPower);
     }
 
     @Override
     public void end(boolean interrupted) {
+        driveTrain.stop();
     }
 
 
     @Override
     public boolean isFinished() {
-        return false;
+        return (System.currentTimeMillis() / 1000) >= duration;
     }
     
 }
-*/
